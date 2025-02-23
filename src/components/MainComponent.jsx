4a4813a537e1c05d2864f59e8c4f0b7508d88a8c
raksmeymom <momraksmey5@gmail.com>
2025-02-23 15:39:27 +0700

    Initial commit

---
+++ src/components/MainComponent.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+// import { NavbarComponent } from './NavbarComponent'
+// import { AboutComponent } from './AboutComponent'
+// import { ServiceComponent } from './ServiceComponent'
+// import { PricingComponent } from './PricingComponent'
+// import ContactComponent from './ContactComponent'
+
+
+// const MainComponent = () => {
+//   return (
+//     <div>
+//         <NavbarComponent/>
+//         <AboutComponent/>
+//         <ServiceComponent/>
+//         <PricingComponent/>
+//         <ContactComponent/>
+
+//     </div>
+//   )
+// }
+
+// export default MainComponent
